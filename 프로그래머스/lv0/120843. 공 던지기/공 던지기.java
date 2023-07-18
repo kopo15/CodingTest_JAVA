c4d80bf65aa069fe0d4b3b3109959f097b151d42
kopo15 <127267673+kopo15@users.noreply.github.com>
2023-07-18 18:47:11 +0900

    Update 공 던지기.java

--- 프로그래머스/lv0/120843. 공 던지기/공 던지기.java
+++ 프로그래머스/lv0/120843. 공 던지기/공 던지기.java
@@ -10,3 +10,13 @@ class Solution {
         return answer;
     }
 }
+
+/*
+반장님 정답 
+class Solution {
+    public int solution(int[] numbers, int k) {
+        return numbers[((k - 1) * 2) % numbers.length]; 
+    }
+}
+ k -1 -> index를 구하기 위해 -1해줌(인덱스는 0부터 시작)
+*/
